a7f23ce2d9d8f49d276952281041fe2e21c8797c
Richard <richardma1999@gmail.com>
2024-02-08 03:26:37 -0500

    added project placeholders

--- richardma-ca-v1/src/app/page.tsx
+++ richardma-ca-v1/src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
       <div id="experience" className="mt-40 px-40">
         <div className="text-4xl w-fit">Experience</div>
         <div className="flex mt-4 p-4 flex-wrap border-corner">
-          <div className="basis-2/5 h-full m-auto border-corner ">
+          <div className="basis-2/5 h-full m-auto">
             <img className="h-3/4 w-auto flex justify-center" src={deckerGif.src}/>
           </div>
           <div className="basis-3/5 p-4">
@@ -109,8 +109,68 @@ export default function Home() {
       </div>
       <div id="project" className="mt-40 px-40">
         <div className="text-5xl border-b-4 w-fit">Projects</div>
-        <div>
-          x
+
+        <div className="flex mt-4 p-4 flex-wrap border-corner">
+          <div className="basis-3/5 p-4">
+            <div className="flex justify-between">
+              <div className="font-bold">Intermediate Front-End Developer</div>
+              <div>Jun 2022-May 2023</div>
+            </div>
+            <div className="flex justify-between">
+              <div className="text-zinc-400">Junior Full-Stack Developer</div>
+              <div className="italic">DeckerMed</div>
+            </div>
+            <p className="mt-4">
+              Responsible for independently rebuilding 35% of total company web pages and 
+              applications using React, utilized by over 25k unique monthly users 
+              from medical programs at top universities and institutions.
+            </p>
+          </div>
+          <div className="basis-2/5 h-full m-auto">
+            <img className="h-3/4 w-auto flex justify-center" src={deckerGif.src}/>
+          </div>
+        </div>
+
+        <div className="flex mt-4 p-4 flex-wrap border-corner">
+          <div className="basis-2/5 h-full m-auto">
+            <img className="h-3/4 w-auto flex justify-center" src={deckerGif.src}/>
+          </div>
+          <div className="basis-3/5 p-4">
+            <div className="flex justify-between">
+              <div className="font-bold">Intermediate Front-End Developer</div>
+              <div>Jun 2022-May 2023</div>
+            </div>
+            <div className="flex justify-between">
+              <div className="text-zinc-400">Junior Full-Stack Developer</div>
+              <div className="italic">DeckerMed</div>
+            </div>
+            <p className="mt-4">
+              Responsible for independently rebuilding 35% of total company web pages and 
+              applications using React, utilized by over 25k unique monthly users 
+              from medical programs at top universities and institutions.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex mt-4 p-4 flex-wrap border-corner">
+          <div className="basis-3/5 p-4">
+            <div className="flex justify-between">
+              <div className="font-bold">Intermediate Front-End Developer</div>
+              <div>Jun 2022-May 2023</div>
+            </div>
+            <div className="flex justify-between">
+              <div className="text-zinc-400">Junior Full-Stack Developer</div>
+              <div className="italic">DeckerMed</div>
+            </div>
+            <p className="mt-4">
+              Responsible for independently rebuilding 35% of total company web pages and 
+              applications using React, utilized by over 25k unique monthly users 
+              from medical programs at top universities and institutions.
+            </p>
+          </div>
+          <div className="basis-2/5 h-full m-auto">
+            <img className="h-3/4 w-auto flex justify-center" src={deckerGif.src}/>
+          </div>
         </div>
       </div>
       <div id="footer" className="mt-40">
