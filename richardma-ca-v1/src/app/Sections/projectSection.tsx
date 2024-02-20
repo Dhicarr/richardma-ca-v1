f22fae5f83d6ff435938bec8393bfe48f6d088dc
Richard <richardma1999@gmail.com>
2024-02-20 06:33:35 -0500

    even more mobile responsiveness

--- richardma-ca-v1/src/app/Sections/projectSection.tsx
+++ richardma-ca-v1/src/app/Sections/projectSection.tsx
@@ -13,7 +13,7 @@ const ProjectSection = ({sectionClass}:ComponentProps): JSX.Element => {
       <div className="text-3xl md:text-4xl">Projects</div>
 
       <div className="flex flex-col justify-center align-middle content-center w-full">
-        <div className="flex mt-4 p-4 flex-wrap border-corner">
+        <div className="flex mt-4 p-2 flex-wrap border-corner">
           <div className="md:basis-3/5 md:p-4 mb-4 lg:mb-0">
             <div className="font-bold">Placeholder</div>
             <div className="flex justify-between">
--- richardma-ca-v1/src/app/Sections/titleSection.tsx
+++ richardma-ca-v1/src/app/Sections/titleSection.tsx
@@ -8,12 +8,13 @@ import DarkSky from "../Components/darkSky";
 
 const TitleSection = (): JSX.Element => {
   return (
-    <main className="box relative w-full z-1 overflow-hidden justify-center mb-4 p-12 text-center md:text-left
-     lg:justify-normal min-h-screen lg:mb-0 lg:p-0">
+    <main className="box relative flex w-full z-1 overflow-hidden justify-center mb-4 p-12 text-center md:text-left
+     lg:justify-normal lg:mb-0 lg:p-0" style={{height:"110vh"}}>
       <DarkSky/>
-      <div className="flex flex-col-reverse lg:flex-row mt-24 md:mt-10">
-        <div className="flex basis-1/2 h-full justify-center">
-          <div className="flex flex-col justify-center md:mt-24 lg:ml-10">
+      <div className="flex flex-col-reverse w-full lg:flex-row mt-0 md:mt-0 lg:mt-0">
+        <div className="flex lg:basis-1/2 h-full justify-center">
+          <div className="flex flex-col justify-center lg:ml-10">
+            <span className="border-4 py-12 px-2 md:border-none md:p-0">
             <div className="text-xl md:text-3xl ">
               <span className="fade-in-1 opacity-0">Hi</span><span className="fade-in-2 opacity-0">, my name is</span>
             </div>
@@ -26,6 +27,7 @@ const TitleSection = (): JSX.Element => {
               <h3>HBSc Computer Science • University of Toronto</h3>
               <JobTitle/>
             </div>
+            </span>
             <div className="fade-in-5 opacity-0 mt-20 ml-auto mr-auto">
               <div className="animate-bounce">
                 <a className="hover:text-purple-500 transition-colors" href="#about">
@@ -36,7 +38,7 @@ const TitleSection = (): JSX.Element => {
           </div>
         </div>
 
-        <div className="hidden md:flex basis-1/2 flex-col justify-center fade-in-right opacity-0 h-full items-center">
+        <div className="hidden lg:-mt-16 lg:flex lg:basis-1/2 flex-col justify-center fade-in-right opacity-0 h-full items-center">
           <NavBar/>
           <img className="white-shadow h-80 lg:h-80 xl:h-96 w-auto border-4" src={image1.src} />
         </div>
