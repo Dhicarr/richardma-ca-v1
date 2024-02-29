5da010789b2053fc1a1d5e9f763779725fa84eef
Richard <richardma1999@gmail.com>
2024-02-29 14:59:10 -0500

    removed google analytics

--- richardma-ca-v1/src/app/layout.tsx
+++ richardma-ca-v1/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./Css/globals.css";
 import "./Css/animation.css";
-import GoogleAnalytics from "./googleAnalytics";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 
@@ -22,7 +21,6 @@ export default function RootLayout({
     <html lang="en">
       <Analytics/>
       <SpeedInsights/>
-      <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID as string} />
       <body className={raleway.className}>{children}</body>
     </html>
   );
