d919c3a7faffc26de6a15b278375f5b680abc7b5
Richard <richardma1999@gmail.com>
2024-02-29 14:58:18 -0500

    added vercel analytics

--- richardma-ca-v1/src/app/layout.tsx
+++ richardma-ca-v1/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./Css/globals.css";
 import "./Css/animation.css";
 import GoogleAnalytics from "./googleAnalytics";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const raleway = Raleway({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics/>
       <SpeedInsights/>
       <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID as string} />
       <body className={raleway.className}>{children}</body>
